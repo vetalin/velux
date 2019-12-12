cdadf2dc325bca66796f7a319e53e1993f6962b4
vetal <vy@napoleonit.ru>
2019-12-12 17:23:10 +0300

    + add fun to createStore

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,1 +1,11 @@
-export const createStore = () => {}
+import { IAction, IReducer, IState, IStore } from 'src/interfaces'
+
+export const createStore = (store: IStore, initialState: IState = {}) => {
+  let state = initialState
+  return {
+    dispatch: async (action: IAction, payload: IReducer[typeof action]) => {
+      state = store.reducer[action](state, payload)
+    },
+    getState: () => state
+  }
+}
